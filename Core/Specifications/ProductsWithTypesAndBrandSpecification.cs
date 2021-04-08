344c53a3b2aadf82cd1b2028972732609cc18f85
78HP <78HP@78HP-PC>
2021-04-08 11:26:06 +0100

    End of section 6

--- Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
@@ -6,12 +6,42 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandSpecification()
+        public ProductsWithTypesAndBrandSpecification(ProductSpecParams productParams)
+            :base( x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                (productParams.Search)) && 
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId) &&
+                (!productParams.CategoryId.HasValue || x.ProductCategoryId == productParams.CategoryId)&&
+                (!productParams.SourceId.HasValue || x.ProductSourceId == productParams.SourceId)
+
+            )
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductCategory);
             AddInclude(x => x.ProductSource);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize *  (productParams.PageIndex - 1),
+            productParams.PageSize);
+
+
+            if(!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    
+                    case "priceAsc": 
+                            AddOrderBy(p => p.Price);
+                            break;
+                    case "priceDesc": 
+                            AddOrderByDescending(p => p.Price);
+                            break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandSpecification(int id) 
