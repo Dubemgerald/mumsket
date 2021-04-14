ed01be5f99d9149d5e7e0ccbe209ec7441880e9c
78HP <78HP@78HP-PC>
2021-04-14 13:53:06 +0100

    End of section 8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,12 @@
+ export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    zoomPictureUrl: string;
+    productType: string;
+    productBrand: string;
+    productCategory: string;
+    productSource: string;
+}
