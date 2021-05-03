358d979d548b5d044c2ce50d3b80db26d07c0205
78HP <78HP@78HP-PC>
2021-05-03 14:49:21 +0100

    End of section 17 client validation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 
@@ -13,10 +14,26 @@ export class AppComponent implements OnInit{
   title = 'Mumsket';
   
 
-  constructor(private basketService: BasketService)  {}
+  constructor(private basketService: BasketService, private accountService: AccountService)  {}
 
   ngOnInit(): void {
-   const basketId = localStorage.getItem('basket_id');
+   this.loadBasket();
+   this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      });
+    }
+  
+
+
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
    if (basketId) {
      this.basketService.getBasket(basketId).subscribe(() => {
        console.log('initialised basket');
