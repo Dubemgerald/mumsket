7b9d76a80ff049c44a16a88c69f0bcec30c92d74
78HP <78HP@78HP-PC>
2021-05-05 18:01:22 +0100

    End of section 18 Orders

---
+++ Core/Interfaces/IOrderService.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities.OrderAggregate;
+
+namespace Core.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId,
+         Address shippingAddress);
+
+        Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
+        Task<Order> GetOrderByIdAsync(int Id, string buyerEmail);
+        Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
+    }
+}
